cb19738d888657a6207b21708723e89befb0989d
rohannj1993 <rohannj1993@gmail.com>
2021-07-12 19:59:32 -0400

    added additional screenshot

--- README.md
+++ README.md
@@ -3,10 +3,13 @@ Our task was to create a application that can be used to write and save notes. T
 Success- Succesfully installed packages
          Deployed to Heroku
          Correct File structure
+         Can Write Notes
 
 Needs improvement- 
 Still need to create correct api routes format so that Notes could render
 
-I also included a screenshot of the landing page with the generated html page-
+I also included screenshots of the landing page with the generated html page-
 
 ![screenshot](public/assets/Screenshot65.png)
+![screenshot](public/assets/Screenshot66.png)
+![screenshot](public/assets/Screenshot67.png)
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,30 +1,29 @@
-const notes = require (`../db/db.json`);
-const { createNewNote} = require(`.json`);
+const notes = require(`../db/db.json`);
 const router = require(`express`).Router();
 const fs = require(`fs`);
 
-router.get(`/notes`,(req,res) => {
-/* 
-using notes run a function that will READ all the notes and then respond back with the notes it finds to put them on the page.
-*/
+router.get(`/notes`, (req, res) => {
+    /* 
+    using notes run a function that will READ all the notes and then respond back with the notes it finds to put them on the page.
+    */
+
+    const notes = JSON.parse(fs.readFileSync(`./db/db.json`, "utf-8"));
+    res.json(notesData);
     
-    const note = JSON.parse(fs.readFileSync(`./db/db.json`),"utf-8");
-res.json(notesData);
-console.log(notesData);
 });
 
-router.post("/notes",(req,res) =>{
+router.post("/notes", (req, res) => {
 
     // using notes run a function that will create a new note either using a spread operator or using an object created from the data receieved from the front end. 
     //HINT console log the request.body to create the object or use the spread operator. 
-    const note = createNewNote(req.body,notes);
+    const note = createNewNote(req.body, notes);
     res.json(note);
     console.log(note);
 });
 
 
-router.delete("/notes/:id",(req,res) =>{
-    
+router.delete("/notes/:id", (req, res) => {
+
 })
 
 
